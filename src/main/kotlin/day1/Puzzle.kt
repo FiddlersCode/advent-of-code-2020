d1e16cfd131550c0364f58bc9199b2a3e62620c3
Paula Muldoon <muldoon@bryter.io>
2020-12-01 09:53:01 +0000

    found teh result, very manky code

--- src/main/kotlin/day1/Puzzle.kt
+++ src/main/kotlin/day1/Puzzle.kt
@@ -3,24 +3,31 @@ package day1
 import java.io.File
 import java.lang.Exception
 
-class Puzzle {
+class Puzzle(private val targetYear: Int = 2020) {
 
-    fun solve(inputFile: String): Int {
-        val numbers = readInputFile(inputFile).map { it.toInt() }
+    fun solve(inputFileName: String): Int {
+        val file = ClassLoader.getSystemClassLoader().getResource(inputFileName).file
+        val numbers = readInputFile(file).map { it.toInt() }
         return solve(numbers)
     }
 
     fun solve(input: List<Int>): Int {
         var result: Int? = null
-        input.mapIndexed { index: Int, number: Int ->
-            if (index + 1 < input.size && number + input[index + 1] == 2020)
-                result = number * input[index + 1]
-
+        input.mapIndexed { index: Int, _: Int ->
+            val list = input.slice(IntRange(index, input.size - 1))
+            val newList = list.filter { isMatch(it, list) }
+            if (newList.isNotEmpty()) {
+                result = newList[0] * newList[1]
+            }
         }
         if (result != null) {
             return result as Int
         } else
             throw Exception("Your input did not contain any pairs which added up to 2020! The horror!")
+    }
+
+    private fun isMatch(number: Int, input: List<Int>): Boolean {
+        return input.any { it + number == targetYear }
     }
 
     private fun readInputFile(filePath: String): List<String> {
--- src/test/kotlin/day1/PuzzleTest.kt
+++ src/test/kotlin/day1/PuzzleTest.kt
@@ -21,4 +21,22 @@ class PuzzleTest {
         val expected = 55776
         assertEquals(expected, actual)
     }
+
+    @Test
+    fun solveTest2() {
+        val input: List<Int> = listOf(
+            1, 5, 3, 28, 843, 23, 1992, 33
+        )
+        val actual: Int = puzzle.solve(input)
+        val expected = 55776
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun solveTest3() {
+        val inputFile = "day1/input.log"
+        val actual: Int = puzzle.solve(inputFile)
+        val expected = 41979
+        assertEquals(expected, actual)
+    }
 }
