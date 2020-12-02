7c5f5ab12fdf0f85015619f01cd920b19ff39ebe
Paula Muldoon <muldoon@bryter.io>
2020-12-02 07:29:42 +0000

    solve part 1

--- src/main/kotlin/day2/Puzzle.kt
+++ src/main/kotlin/day2/Puzzle.kt
@@ -3,17 +3,63 @@ package day2
 import java.io.File
 import java.lang.Exception
 
-class Puzzle(private val targetYear: Int = 2020) {
+data class ParsedInputLine(
+    val frequency: IntRange,
+    val letter: Char,
+    val password: CharArray
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
 
+        other as ParsedInputLine
 
-    fun parseInputLine(input: String): Map<String, String> {
+        if (frequency != other.frequency) return false
+        if (letter != other.letter) return false
+        if (!password.contentEquals(other.password)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = frequency.hashCode()
+        result = 31 * result + letter.hashCode()
+        result = 31 * result + password.contentHashCode()
+        return result
+    }
+}
+
+class Puzzle {
+
+    fun solve(inputFileName: String): Int {
+        val file = ClassLoader.getSystemClassLoader().getResource(inputFileName).file
+        val parsedFileLines: List<ParsedInputLine> = readInputFile(file).map { parseInputLine(it) }
+        return parsedFileLines.filter { isValid(it) }.size
+    }
+
+    fun isValid(input: ParsedInputLine): Boolean {
+        val numberOfOccurrences = numberOfOccurrences(input.password, input)
+        return input.frequency.contains(numberOfOccurrences)
+    }
+
+    fun parseInputLine(input: String): ParsedInputLine {
         val parts = input.split(" ", ":", " ")
-        return mapOf(
-            "frequency" to parts[0],
-            "letter" to parts[1],
-            "password" to parts[3]
+        return ParsedInputLine(
+            frequency = convertToIntRange(parts[0]),
+            letter = parts[1].single(),
+            password = parts[3].toCharArray()
         )
     }
+
+    private fun convertToIntRange(input: String): IntRange {
+        val split = input.split("-")
+        return IntRange(split[0].toInt(), split[1].toInt())
+    }
+
+    private fun numberOfOccurrences(password: CharArray, input: ParsedInputLine): Int {
+        return password.filter { it == input.letter }.size
+    }
+
     private fun readInputFile(filePath: String): List<String> {
         return File(filePath).readLines()
     }
--- src/test/kotlin/day2/PuzzleTest.kt
+++ src/test/kotlin/day2/PuzzleTest.kt
@@ -16,10 +16,10 @@ class PuzzleTest {
     fun parseFileTest1() {
         val input = "16-18 h: hhhhhhhhhhhhhhhhhh"
         val actual = puzzle.parseInputLine(input)
-        val expected: Map<String, String> = mapOf(
-            "frequency" to "16-18",
-            "letter" to "h",
-            "password" to "hhhhhhhhhhhhhhhhhh"
+        val expected = ParsedInputLine(
+            frequency = IntRange(16, 18),
+            letter = "h".single(),
+            password = "hhhhhhhhhhhhhhhhhh".toCharArray()
         )
         assertEquals(expected, actual)
     }
@@ -28,10 +28,10 @@ class PuzzleTest {
     fun parseFileTest2() {
         val input = "17-18 d: ddddddddddddddddzn"
         val actual = puzzle.parseInputLine(input)
-        val expected: Map<String, String> = mapOf(
-            "frequency" to "17-18",
-            "letter" to "d",
-            "password" to "ddddddddddddddddzn"
+        val expected = ParsedInputLine(
+            frequency = IntRange(17, 18),
+            letter = "d".single(),
+            password = "ddddddddddddddddzn".toCharArray()
         )
         assertEquals(expected, actual)
     }
@@ -40,11 +40,44 @@ class PuzzleTest {
     fun parseFileTest3() {
         val input = "8-14 v: pvxlknfvplgktv"
         val actual = puzzle.parseInputLine(input)
-        val expected: Map<String, String> = mapOf(
-            "frequency" to "8-14",
-            "letter" to "v",
-            "password" to "pvxlknfvplgktv"
+        val expected = ParsedInputLine(
+            frequency = IntRange(8, 14),
+            letter = "v".single(),
+            password = "pvxlknfvplgktv".toCharArray()
         )
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun isValidTest1() {
+        val input = ParsedInputLine(
+            frequency = IntRange(8, 14),
+            letter = "v".single(),
+            password = "pvxlknfvplgktv".toCharArray()
+        )
+        val actual = puzzle.isValid(input)
+        val expected = false
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun isValidTest2() {
+        val input = ParsedInputLine(
+            frequency = IntRange(3, 7),
+            letter = "q".single(),
+            password = "sffnxxqvdfsnqlhqpq".toCharArray()
+        )
+        val actual = puzzle.isValid(input)
+        val expected = true
+        assertEquals(expected, actual)
+    }
+
+
+    @Test
+    fun solveTest() {
+        val input = "day2/input.log"
+        val actual = puzzle.solve(input)
+        val expected = 465
         assertEquals(expected, actual)
     }
 }
